e75a02e73204c9bbfe40049a5c367c73bf10047f
UvularGecko2125 <70552300+UvularGecko2125@users.noreply.github.com>
2021-03-16 11:18:33 +0800

    Add files via upload

---
+++ sketch_mar16a.ino
@@ -0,0 +1,26 @@
+int void setup() {
+  
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+}
+
+void loop() {
+
+  
+  analogWrite(5,60);
+  analogWrite(6,0);
+  delay(3000);
+
+  analogWrite(5,80);
+  analogWrite(6,0);
+  delay(3000);
+
+  analogWrite(5,100);
+  analogWrite(6,0);
+  delay(3000);
+
+  analogWrite(5,120);
+  analogWrite(6,0);
+  delay(3000);
+
+}
